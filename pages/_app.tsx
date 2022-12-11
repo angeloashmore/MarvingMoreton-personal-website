fe7434bf399e871733aba6b34dadf48557e04319
Marving Moreton <moreton.marving@gmail.com>
2022-12-11 16:40:06 -0500

    added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Lato, Poppins } from "@next/font/google";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
 import Layout from "../components/layout/Layout";
 const lato = Lato({
   subsets: ["latin"],
@@ -14,8 +17,21 @@ const poppins = Poppins({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const canonicalUrl = `https://marvingmoreton.com` + router.asPath;
+
   return (
     <Layout>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/public/marving-moreton-favicon.png"
+        />
+        <link rel="canonical" href={canonicalUrl} />
+      </Head>
       <main className={lato.className}>
         <Component {...pageProps} />
       </main>
