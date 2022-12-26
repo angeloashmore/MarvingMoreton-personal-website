b94ba2d5d2483fe0e752f5137a3e3ab5c64ff501
Marving Moreton <moreton.marving@gmail.com>
2022-12-26 16:55:18 -0500

    fixing bug prismic installation

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
-import HomeBlogHero from "../../components/ui/Blog/HomeBlogHero";
-
+import HomeBlogHero from "../../components/ui/Blog/HomeBlogHero"
 import TextBoxLight from "../../components/ui/Shared/TextBoxLight";
 import ImageFeaturedLeft from "../../components/ui/Shared/ImageFeaturedLeft";
 
