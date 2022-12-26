721a19bd3c2e45e4e81c86b50d659ad72308e173
Marving Moreton <moreton.marving@gmail.com>
2022-12-26 18:49:49 -0500

    removing everything (necessary) from prismic & slicemachine

--- .slicemachine/assets/slices/BlogHero/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/BlogHero';
-
-export default {
-  title: 'slices/BlogHero'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Introduced","spans":[]}],"description":[{"type":"paragraph","text":"Minim incididunt proident consequat Lorem dolore consectetur qui. Duis in sint reprehenderit ex eiusmod magna adipisicing nostrud Lorem.","spans":[]}]},"id":"_Default","slice_type":"blog_hero"}} />
-_Default.storyName = ''
