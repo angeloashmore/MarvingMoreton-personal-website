efd36694c84868f9a69d52cde0b5c3714baaddb6
Marving Moreton <moreton.marving@gmail.com>
2022-12-29 23:36:18 -0500

    responsiveness FAQ

--- slices/Faq/index.js
+++ slices/Faq/index.js
@@ -4,60 +4,115 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { PrismicRichText, PrismicText, PrismicLink } from "@prismicio/react";
-import { PrismicNextImage } from "@prismicio/next";
+import { PrismicRichText, PrismicText, PrismicLink } from '@prismicio/react';
+import { PrismicNextImage } from '@prismicio/next';
 
-import classes from "./Faq.module.css"
+import classes from './Faq.module.css';
 /**
  * @typedef {import("@prismicio/client").Content.FaqSlice} FaqSlice
  * @typedef {import("@prismicio/react").SliceComponentProps<FaqSlice>} FaqProps
  * @param { FaqProps }
  */
-export default function Faq ({slice}) {
-  return (
-    <section className={classes["section-faq"]}>
-      <h2 className="heading-secondary">Frequently Asked Question</h2>
-      <div className={classes["questions-grid"]}>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography className={classes.question}>Accordion 1</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography className={classes.answer}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-              malesuada lacus ex, sit amet blandit leo lobortis eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel2a-content"
-            id="panel2a-header"
-          >
-            <Typography className={classes.question}>Accordion 2</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography className={classes.answer}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-              malesuada lacus ex, sit amet blandit leo lobortis eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        {/* <Accordion disabled>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel3a-content"
-            id="panel3a-header"
-          >
-            <Typography>Disabled Accordion</Typography>
-          </AccordionSummary>
-        </Accordion> */}
-      </div>
-    </section>
-  );
+export default function Faq({ slice }) {
+    return (
+        <section className={classes['section-faq']}>
+            <h2 className="heading-secondary">Frequently Asked Question</h2>
+            <div className={classes['questions-grid']}>
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                        <Typography className={classes.question}>
+                            Accordion 1
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography className={classes.answer}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Suspendisse malesuada lacus ex, sit amet
+                            blandit leo lobortis eget.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel2a-content"
+                        id="panel2a-header"
+                    >
+                        <Typography className={classes.question}>
+                            Accordion 2
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography className={classes.answer}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Suspendisse malesuada lacus ex, sit amet
+                            blandit leo lobortis eget.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel3a-content"
+                        id="panel3a-header"
+                    >
+                        <Typography className={classes.question}>
+                            Accordion 3
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography className={classes.answer}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Suspendisse malesuada lacus ex, sit amet
+                            blandit leo lobortis eget.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel4a-content"
+                        id="panel4a-header"
+                    >
+                        <Typography className={classes.question}>
+                            Accordion 4
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography className={classes.answer}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Suspendisse malesuada lacus ex, sit amet
+                            blandit leo lobortis eget.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel5a-content"
+                        id="panel5a-header"
+                    >
+                        <Typography className={classes.question}>
+                            Accordion 5
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography className={classes.answer}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Suspendisse malesuada lacus ex, sit amet
+                            blandit leo lobortis eget.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            </div>
+        </section>
+    );
 }
