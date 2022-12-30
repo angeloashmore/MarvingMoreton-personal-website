891b9014c2ccce26ef93508fa1a9d47f86f5cf22
Marving Moreton <moreton.marving@gmail.com>
2022-12-29 19:00:07 -0500

    CTA Horizontal responsiveness

--- slices/CtaHorizontal/index.js
+++ slices/CtaHorizontal/index.js
@@ -9,27 +9,6 @@ import Link from "next/link";
  * @param { CtaHorizontalProps }
  */
 const CtaHorizontal = ({ slice }) => (
-  // <React.Fragment>
-  //     <section className={classes["section--footer"]}>
-  //       <div className={classes["cta-container"]}>
-  //         <div className={classes["cta-banner"]}>
-  //           <div className={classes["cta-text-box"]}>
-  //             <PrismicRichText field={slice.primary.title} />
-  //             <p >{ slice.primary.text }</p>
-  //           </div>
-  //           <div className={classes["cta-text-box"]}>
-              // <PrismicLink  field={slice.primary.cta_link} className="btn-dark btn"
-              //   href="mailto:marving.moreton@gmail.com"
-              //   target="_blank">{ slice.primary.cta_anchor }
-              // </PrismicLink>
-
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </section>
-  // </React.Fragment>
-
-
   <React.Fragment>
     <section className={classes["section--footer"]}>
       <div className={classes["cta-container"]}>
